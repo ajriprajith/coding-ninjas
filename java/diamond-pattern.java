9dd60048016552a368c7835f79e296da3513db17
Ajri <ajriprajithcoc1@gmail.com>
2023-10-10 17:17:54 +0530

    working pritting odd incremented diamond patter

---
+++ java/diamond-pattern.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class DiamondPattern {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int rows = scanner.nextInt();
+
+        int spaces = rows / 2;
+        int stars = 1;
+        // System.out.println(spaces);
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+            if (i <= rows / 2) {
+                spaces--;
+                stars += 2;
+            } else {
+                spaces++;
+                stars -= 2;
+            }
+        }
+    }
+}
