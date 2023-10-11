277ba33c7a82b93307d553fda3825bf6a2f731f0
Ajri <ajriprajithcoc1@gmail.com>
2023-10-11 19:30:06 +0530

    printing in triangular pattern

---
+++ java/addnumber-in-pattern.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class addnumber {
+    public static void main(String[] args){
+        
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt(), i = 1, sum=1;
+        while(i<=n){
+            int j=1;
+            while(j<=i){
+                if(j==1){
+                    System.out.print(j);   
+                }
+                else{
+                    System.out.print("+"+j);
+                }
+                j++;
+        }
+        System.out.print("="+ sum);
+        System.out.println();
+        i++;
+        sum = sum + i;
+    }
+}
+}
+
--- java/test.java
+++ java/test.java
@@ -1,5 +1,55 @@
+import java.util.Scanner;
 class solution{
     public static void main(String args[]){
-        System.out.println("noting");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            // Print spaces before the first asterisk
+
+            // Print numbers in increasing order
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            // Print numbers in decreasing order, excluding the last number (1 for the first row)
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+
+            // Print spaces after the last number (1 for the first row)
+           
+
+            System.out.println();
+        }
+
+        // while(i<=n*2+1){
+        //     int j = 1;
+        //     while(j<=cols){
+        //         int print = 1;
+        //         if (j==1 || j==cols)
+        //             System.out.print("x");
+        //         else if((cols/2)+1<=j)   
+        //         {        
+        //             System.out.print(print);  
+        //             print++;
+        //         }
+        //         else{
+        //             System.out.print(print);
+        //             print--;
+        //         }
+        //         j++;
+        //     }
+        //     if(i<=((n*2+1)/2)+1){
+        //         cols+=2;
+        //     }
+        //     else{
+        //         cols-=2;
+        //         System.out.println();
+        //     }
+        //     i++;
+        //     System.out.println(j);
+            
+        // }
     }
 }
